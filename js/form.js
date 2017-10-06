8fd3809fd2f72bea4314f9ac039207ed7920f467
svante.rohlin <svante.rohlin@21grams.com>
2017-10-06 14:54:35 +0200

    Remove logging

--- js/form.js
+++ js/form.js
@@ -67,8 +67,8 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     // xhr.withCredentials = true;
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function () {
-        console.log(xhr.status, xhr.statusText)
-        console.log(xhr.responseText);
+        // console.log(xhr.status, xhr.statusText)
+        // console.log(xhr.responseText);
         document.getElementById('gform').style.display = 'none'; // hide form
         document.getElementById('thankyou_message').style.display = 'block';
         return;
